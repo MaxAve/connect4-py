2d24d9db8046e629eede54bf23c82013e319d085
MaxAve <maxave909@gmail.com>
2024-05-28 19:42:58 +0200

    Add a few trained models

--- connect4.py
+++ connect4.py
@@ -21,11 +21,11 @@ class Board:
                 if column[i] == Board.X:
                     board_data += "X "
                 elif column[i] == Board.O:
-                    board_data += "O "
+                    board_data += "\033[93mO\033[0m "
                 else:
                     board_data += "- "
             board_data += "\n"
-        return f"0 1 2 3 4 5 6\n{board_data}"
+        return f"1 2 3 4 5 6 7\n{board_data}"
     
     def is_column_free(self, column):
         return self.free_cells[column] > -1
@@ -38,6 +38,9 @@ class Board:
         return fcols
 
     def place(self, column, piece):
+        if not self.is_column_free(column):
+            self.place(self.get_free_columns[0])
+            return
         self.board[column][self.free_cells[column]] = piece
         self.free_cells[column] -= 1
     
@@ -55,16 +58,16 @@ class Board:
                     if self.board[col][row] == self.board[col + 1][row] == self.board[col + 2][row] == self.board[col + 3][row]:
                         return self.board[col][row]
         # /
-        for row in range(3):
-            for col in range(3):
-                if self.board[col][row] != Board.NONE:
-                    if self.board[col][row] == self.board[col + 1][row + 1] == self.board[col + 2][row + 2] == self.board[col + 3][row + 3]:
-                        return self.board[col][row]
-        # \
         for row in range(3, 6):
             for col in range(3):
                 if self.board[col][row] != Board.NONE:
                     if self.board[col][row] == self.board[col + 1][row - 1] == self.board[col + 2][row - 2] == self.board[col + 3][row - 3]:
+                        return self.board[col][row]
+        # \
+        for row in range(3):
+            for col in range(3):
+                if self.board[col][row] != Board.NONE:
+                    if self.board[col][row] == self.board[col + 1][row + 1] == self.board[col + 2][row + 2] == self.board[col + 3][row + 3]:
                         return self.board[col][row]
         return Board.NONE
     
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from connect4 import *
 import computer_move
-from evaluation import heuristics
-from evaluation import neural_network
-import threading
+import heuristics
+import neural_network
+import os
+from copy import deepcopy
+import torch
 
+# Plays a game between the given model and a preprogrammed heuristic model
+# Use this during initial training stages
 def train_with_heuristics(model, score_tally, game_id):
     board = Board()
     computer_move.set_evaluation_function(heuristics.eval)
@@ -14,11 +18,53 @@ def train_with_heuristics(model, score_tally, game_id):
             # Computer move (heuristics)
             best_move = computer_move.minimax(board, 4, True, float("-inf"), float("inf"))
             board.place(best_move[1], Board.X)
+            if not (board.get_winner() == Board.NONE and board.is_draw() == False):
+                break
             # Computer move (neural network)
             best_move = model.minimax(board, 4, False, float("-inf"), float("inf"))
             board.place(best_move[1], Board.O)
 
             moves_played += 1
+        print(board)
+        print(board.get_winner())
+    except:
+        print(f"Game No. {game_id} exited with an error.")
+        score_tally[game_id] = -1
+        return
+
+    # Calculate the score
+    # This will save a number between -1 and 1 (-1/1 = lost/won game before it even started)
+    if board.is_draw():
+        score = 0
+    elif board.get_winner() == Board.X:
+        # Reduce penalty for games that lasted longer
+        score = (moves_played - 42) / 42
+    elif board.get_winner() == Board.O:
+        # Reduce score for games that lasted longer
+        score = (42 - moves_played) / 42
+    score_tally[game_id] = score
+    print(f"Finished game No. {game_id}. The model had an overall playing strength of {score} ({int(score*100)}%).")
+
+# Has 2 models play a round against eachother
+def train_with_model(model1, model2, score_tally, game_id):
+    board = Board()
+    computer_move.set_evaluation_function(heuristics.eval)
+    moves_played = 0
+
+    try:
+        while board.get_winner() == Board.NONE and board.is_draw() == False:
+            # Computer move (heuristics)
+            best_move = model1.minimax(board, 4, False, float("-inf"), float("inf"))
+            board.place(best_move[1], Board.X)
+            if not (board.get_winner() == Board.NONE and board.is_draw() == False):
+                break
+            # Computer move (neural network)
+            best_move = model2.minimax(board, 4, False, float("-inf"), float("inf"))
+            board.place(best_move[1], Board.O)
+
+            moves_played += 1
+        print(board)
+        print(board.get_winner())
     except:
         print(f"Game No. {game_id} exited with an error.")
         score_tally[game_id] = -1
@@ -39,37 +85,47 @@ def train_with_heuristics(model, score_tally, game_id):
 
 def main():
     board = Board()
-
+    '''
     breedable_population_size = 5 # Amount of models that will be allowed to breed after each round
     offspring_population_size = int(breedable_population_size * (breedable_population_size - 1))
 
     # Create initial population
     active_models = []
+    model = neural_network.NN()
+    model.load_state_dict(torch.load("models/nn-1.pth"))
+    model.eval()
     for _ in range(offspring_population_size):
-        model = neural_network.NN()
         active_models.append(model)
 
-    for r in range(5):
+    winner_model = None
+
+    for r in range(10):
         print(f"Starting round {r+1}.")
 
         score_tally = [0] * len(active_models)
 
         # Let every model play 1 round each with the heuristics bot
+        # for i in range(len(active_models)):
+        #     train_with_heuristics(active_models[i], score_tally, i)
+
         for i in range(len(active_models)):
-            train_with_heuristics(active_models[i], score_tally, i)
+            train_with_model(active_models[i], winner_model, score_tally, i)
 
         # Pick out the best ones
         best_models = []
+        score_tally_tmp = deepcopy(score_tally)
         while len(best_models) < breedable_population_size:
             max_score = float("-inf")
             best_model_index = 0
             for i in range(len(active_models)):
-                if score_tally[i] > max_score:
-                    max_score = score_tally[i]
+                if score_tally_tmp[i] > max_score:
+                    max_score = score_tally_tmp[i]
                     best_model_index = i
+            if len(best_models) == 0:
+                winner_model = active_models[best_model_index]
             best_models.append(active_models[best_model_index])
             del active_models[best_model_index]
-            del score_tally[best_model_index]
+            del score_tally_tmp[best_model_index]
         active_models.clear()
 
         # Calculate average
@@ -78,7 +134,6 @@ def main():
             score_sum += score
         print(f"Average playing strength: {score_sum / float(len(score_tally))} ({int(score_sum / float(len(score_tally))*100)}%)")
         print(f"Best: {max(score_tally)} ({int(max(score_tally)*100)}%)")
-        print(f"Worst: {min(score_tally)} ({int(min(score_tally)*100)}%)")
 
         # Breed everybody with everybody else
         print("Breeding...")
@@ -86,43 +141,49 @@ def main():
             for j in range(len(best_models)):
                 if i != j:
                     active_models.append(neural_network.breed(best_models[i], best_models[j], 0.05))
+        #active_models += best_models # We want to keep using the winners from the last generation to prevent accidental devolution
 
-        continue_training = input("Continue? [Y/N]: ")
-        if continue_training.lower() == "n":
-            break
+        # continue_training = input("Continue? [Y/N]: ")
+        # if continue_training.lower() == "n":
+        #     break
 
-    # computer_move.set_evaluation_function(heuristics.eval)
-    
-    # while board.get_winner() == Board.NONE and board.is_draw() == False:
-    #     print(board)
-    #     print(f"Shallow eval: {computer_move.evlauation_function(board)}")
+    # Save the model
+    torch.save(winner_model.state_dict(), "/home/maxave/Desktop/connect4-py/models/nn-3.pth")
+    '''
 
-    #     # Player move
-    #     # col = int(input("COL: "))
-    #     # col = min(6, max(0, col)) # Only columns 0 - 6
-    #     # if board.is_column_free(col):
-    #     #     board.place(col, Board.X) # Place piece at the selected column
-    #     # else:
-    #     #     print(f"Column {col} is not free. Please choose another one.")
-    #     #     continue
+    loaded_model = neural_network.NN()
+    loaded_model.load_state_dict(torch.load("models/nn-3.pth"))
+    loaded_model.eval()
 
-    #     computer_move.set_evaluation_function(heuristics.eval)
-    #     cpu_move = computer_move.minimax(board, 4, True, float("-inf"), float("inf"))
-    #     board.place(cpu_move[1], Board.X)
+    # Player vs AI
+    while board.get_winner() == Board.NONE and board.is_draw() == False:
+        os.system("clear")
+        print(board)
 
-    #     # Computer move
-    #     print("Computer is thinking...")
-    #     computer_move.set_evaluation_function(neural_network.eval)
-    #     cpu_move = computer_move.minimax(board, 4, False, float("-inf"), float("inf"))
-    #     print(f"Deep eval: {cpu_move[0]}; Best move: {cpu_move[1]}")
-    #     board.place(cpu_move[1], Board.O)
+        # Player move
+        col = int(input("COL: "))
+        col = min(7, max(1, col)) # Only columns 1 - 7
+        if board.is_column_free(col - 1):
+            board.place(col - 1, Board.X) # Place piece at the selected column
+        else:
+            print(f"Column {col - 1} is not free. Please choose another one.")
+            continue
+        
+        os.system("clear")
+        print(board)
 
-    # # End of game
-    # print(board)
-    # if board.is_draw():
-    #     print("IT'S A DRAW!")
-    # else:
-    #     print("PLAYER 1 (X) WINS!" if board.get_winner() == Board.X else "PLAYER 2 (O) WINS!")
+        # Computer move
+        print("Computer is thinking...")
+        cpu_move = loaded_model.minimax(board, 6, False, float("-inf"), float("inf"))
+        print(f"Deep eval: {cpu_move[0]}\nBest move: {cpu_move[1]}")
+        board.place(cpu_move[1], Board.O)
+
+    # End of game
+    print(board)
+    if board.is_draw():
+        print("IT'S A DRAW!")
+    else:
+        print("PLAYER 1 (X) WINS!" if board.get_winner() == Board.X else "PLAYER 2 (O) WINS!")
 
 
 if __name__ == "__main__":
